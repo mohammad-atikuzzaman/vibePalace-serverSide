2baadd166051c384da286a9938c32eda678eb39d
mr-codex <mcodex47@gmail.com>
2024-05-14 21:45:27 +0600

    jwt installed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
@@ -10,8 +11,6 @@ app.use(cors());
 
 const uri =
   `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.tyigyp7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// const uri =
-//   "mongodb+srv://vibePalace:akash123@cluster0.tyigyp7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,6 +23,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+  //  jwt token
+
+
+
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // await client.db("admin").command({ ping: 1 });
@@ -43,7 +50,6 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
 
     app.get("/reviews", async (req, res) => {
       const sortOption = { time: -1 };
@@ -77,8 +83,6 @@ async function run() {
 
       res.send(result);
     });
-
-
 
     app.post("/bookings", async (req, res) => {
       const data = req.body;
@@ -132,7 +136,6 @@ async function run() {
 
     app.delete("/deleteBookings/:id", async(req, res)=>{
       const id = req.params.id;
-      // console.log(id)
       const query = {_id: id}
       const result = await bookings.deleteOne(query)
       res.send(result)
